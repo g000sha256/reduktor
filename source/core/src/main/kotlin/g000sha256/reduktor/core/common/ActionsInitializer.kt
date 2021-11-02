75c95faeec8fd325eef84652e906dc265fd69e11
g000sha256 <g000sha256@gmail.com>
2021-11-02 21:10:45 +0300

    Version 0.2

--- source/core/src/main/kotlin/g000sha256/reduktor/core/common/ActionsInitializer.kt
+++ source/core/src/main/kotlin/g000sha256/reduktor/core/common/ActionsInitializer.kt
@@ -7,11 +7,35 @@ import g000sha256.reduktor.core.Initializer
 class ActionsInitializer<ACTION, STATE> : Initializer<ACTION, STATE> {
 
     val actions: Actions<ACTION>
-        get() = synchronized(any) { _actions ?: throw IllegalStateException("Initializer has not been called yet") }
 
     private val any = Any()
 
     private var _actions: Actions<ACTION>? = null
+
+    init {
+        actions = object : Actions<ACTION> {
+
+            override fun post(action: ACTION) {
+                post { it.post(action) }
+            }
+
+            override fun post(vararg actions: ACTION) {
+                post { it.post(*actions) }
+            }
+
+            override fun post(actions: Iterable<ACTION>) {
+                post { it.post(actions) }
+            }
+
+            private fun post(callback: (Actions<ACTION>) -> Unit) {
+                synchronized(any) {
+                    val actions = _actions ?: throw IllegalStateException("Initializer has not been called yet")
+                    callback(actions)
+                }
+            }
+
+        }
+    }
 
     override fun Environment<ACTION>.invoke(initialState: STATE) {
         synchronized(any) {
